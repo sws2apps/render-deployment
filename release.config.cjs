f5e75e40a6344a4208670b8cd150d2905a723a6e
rhahao <26148770+rhahao@users.noreply.github.com>
2025-09-19 21:16:11 +0300

    chore(actions): restore required files for backend

--- release.config.cjs
+++ release.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md'],
+        assets: ['package.json', 'CHANGELOG.md'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
