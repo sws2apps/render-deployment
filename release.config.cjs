9ce23c1f0100fdc239842ce46c2ea6bf2142aa31
rhahao <26148770+rhahao@users.noreply.github.com>
2025-09-19 20:54:38 +0300

    fix(ci): apply coderabbit fixes

--- release.config.cjs
+++ release.config.cjs
@@ -15,12 +15,6 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
-    [
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md'],
